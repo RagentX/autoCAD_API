18b3857f984c06edc855a2d590788d301d817b50
RagentX <71557636+RagentX@users.noreply.github.com>
2022-09-09 22:26:21 +0300

    Add files via upload

--- ClientClap/ClientClap/Form1.cs
+++ ClientClap/ClientClap/Form1.cs
@@ -16,47 +16,45 @@ namespace ClientClap
 {
     public partial class Form1 : Form
     {
-        List<string> excelPar = new List<string>();
-        string filename;
+        List<String> excelPar = new List<String>();
+        String filename;
+
+        // Возмыжные варианты размеров DN.
+        int[] values = { 20, 25, 40, 50, 80, 100, 150, 200 };
         public Form1()
         {
             InitializeComponent();
         }
-        private async void button1_Click(object sender, EventArgs e)
-        {
 
-
-
-        }
-        private async Task<String> RequestAsync(string filename, string hand, string parClap, string ram_par, string priv_par, string backParts, string frontParts)
+        private async Task<String> RequestAsync(String filename, String hand, String valveParameters, String frameParameters, String actuator_par, String backParts, String frontParts)
         {
             StreamReader reader = null;
             try
             {
-                string url = $"https://localhost:44394/Index?klap_par={parClap}&klap=Клапан&ram_par={ram_par}&priv_par={priv_par}&"
+                String url = $"https://localhost:44394/Index?klap_par={valveParameters}&klap=Клапан&frameParameters={frameParameters}&actuator_par={actuator_par}&"
                     + $"backParts={backParts}&frontParts={frontParts}&hand={hand}&filename={filename}";
                 WebRequest request = WebRequest.Create(url);
                 WebResponse response = await request.GetResponseAsync().ConfigureAwait(true);
                 Stream stream = response.GetResponseStream();
                 reader = new StreamReader(stream);
                 return await reader.ReadToEndAsync();
-            } finally
+            } lineNumberAccessoriesally
             {
                 reader.Close();
             }
         }
 
 
-        public static string[][] getDataFromCSV(string pathCsvFile)
+        public static String[][] getDataFromCSV(String pathCsvFile)
         {
-            List<string[]> data = new List<string[]>();
+            List<String[]> data = new List<String[]>();
             using (System.IO.StreamReader file = new System.IO.StreamReader(pathCsvFile))
             {
-                string line;
+                String line;
                 while ((line = file.ReadLine()) != null)
                 {
                     String[] parts_of_line = line.Split(';');
-                    string[] mass = new string[parts_of_line.Length];
+                    String[] mass = new String[parts_of_line.Length];
                     for (int i = 0; i < parts_of_line.Length; i++)
                     {
                         parts_of_line[i] = parts_of_line[i].Trim();
@@ -71,11 +69,11 @@ namespace ClientClap
 
 
         //todo remove unused wrapping
-        public static string getData(int value, string form, int class_asme)
+        public static String getData(int value, String form, int class_asme)
         {
-            string[][] dataMassCsvFileA = getDataFromCSV(@"..\..\..\..\csvFiles\A.csv");
-            string[][] dataMassCsvFileB = getDataFromCSV(@"..\..\..\..\csvFiles\B.csv");
-            string[][] dataMassCsvFileC = getDataFromCSV(@"..\..\..\..\csvFiles\C.csv");
+            String[][] dataMassCsvFileA = getDataFromCSV(@"..\..\..\..\csvFiles\A.csv");
+            String[][] dataMassCsvFileB = getDataFromCSV(@"..\..\..\..\csvFiles\B.csv");
+            String[][] dataMassCsvFileC = getDataFromCSV(@"..\..\..\..\csvFiles\C.csv");
 
 
             for (int i = 0; i < dataMassCsvFileA.GetLength(0); i++)
@@ -121,31 +119,31 @@ namespace ClientClap
             }
             return $"{A};{B};{C}";
         }
-        public static string getDataPriv(int size, bool par)
+        public static String getDataPriv(int size, bool par)
         {
             int[] sizes = new int[] { 6, 10, 16, 23 };
             int nomberSize = Array.IndexOf(sizes, size);
-            string[][] dataMassCsvFile = getDataFromCSV(@"..\..\..\..\csvFiles\Priv.csv");
+            String[][] dataMassCsvFile = getDataFromCSV(@"..\..\..\..\csvFiles\Priv.csv");
             int parSize = 2;
             if (par) parSize++;
-            return $"∅{dataMassCsvFile[1 + nomberSize][1]};" +
+            return $"{dataMassCsvFile[1 + nomberSize][1]};" +
                     $"{dataMassCsvFile[1 + nomberSize][parSize]};" +
                     $"{dataMassCsvFile[1 + nomberSize][4]};" +
-                    $"∅{dataMassCsvFile[1 + nomberSize][5]}";
+                    $"{dataMassCsvFile[1 + nomberSize][5]}";
 
         }
         public static int getWeightPriv(int size, bool par)
         {
             int[] sizes = new int[] { 6, 10, 16, 23 };
             int nomberSize = Array.IndexOf(sizes, size);
-            string[][] dataMassCsvFile = getDataFromCSV(@"..\..\..\..\csvFiles\weightsDimen.csv");
+            String[][] dataMassCsvFile = getDataFromCSV(@"..\..\..\..\csvFiles\weightsDimen.csv");
             int parSize = par ? 2 : 1;
             return Convert.ToInt32(dataMassCsvFile[nomberSize + 1][parSize]);
 
         }
         public static int getWeight(int value, int class_asme)
         {
-            string[][] dataMassCsvFileA = getDataFromCSV(@"..\..\..\..\csvFiles\bredWeights.csv");
+            String[][] dataMassCsvFileA = getDataFromCSV(@"..\..\..\..\csvFiles\bredWeights.csv");
             for (int i = 0; i < dataMassCsvFileA[0].GetLength(0); i++)
             {
                 if (dataMassCsvFileA[0][i] == class_asme.ToString())
@@ -156,10 +154,10 @@ namespace ClientClap
             return 0;
 
         }
-        public static string getFrontPart(string Part)
+        public static String getFrontPart(String Part)
         {
             Part = Part.Replace("\"", "").Trim().ToLower();
-            string[][] dataMassCsvFileParts = getDataFromCSV(@"..\..\..\..\csvFiles\artFrontParts.csv");
+            String[][] dataMassCsvFileParts = getDataFromCSV(@"..\..\..\..\csvFiles\artFrontParts.csv");
             List<String> parts = new List<String>();
             List<String> numberPurts = new List<String>();
             for (int i = 0; i < dataMassCsvFileParts.Length; i++)
@@ -171,10 +169,10 @@ namespace ClientClap
             if (a != -1) return numberPurts[a];
             return "0";
         }
-        public static string getBackPart(string Part)
+        public static String getBackPart(String Part)
         {
             Part = Part.Replace("\"", "").Trim().ToLower();
-            string[][] dataMassCsvFileParts = getDataFromCSV(@"..\..\..\..\csvFiles\artBackParts.csv");
+            String[][] dataMassCsvFileParts = getDataFromCSV(@"..\..\..\..\csvFiles\artBackParts.csv");
             List<String> parts = new List<String>();
             List<String> numberPurts = new List<String>();
             for (int i = 0; i < dataMassCsvFileParts.Length; i++)
@@ -199,17 +197,17 @@ namespace ClientClap
             Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(filename, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing); //открыть файл
             Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
             int start = 14;
-            int fin = start;
+            int lineNumberAccessories = start;
             while (true)
             {
-                if (ObjWorkSheet.Cells[fin, 2].Text.ToString() == "") break;
-                listBox1.Items.Add(ObjWorkSheet.Cells[fin, 2].Text.ToString());
-                excelPar.Add("Модель клапана: " + ObjWorkSheet.Cells[fin, 4].Text.ToString() +
-                            "\nМодель привода: " + ObjWorkSheet.Cells[fin, 18].Text.ToString() +
-                            "\nКласс давления: " + ObjWorkSheet.Cells[fin, 8].Text.ToString() + "PN , " +
-                            ObjWorkSheet.Cells[fin, 9].Text.ToString() + " ASME" +
-                            "\nDN: " + ObjWorkSheet.Cells[fin, 11].Text.ToString());
-                fin++;
+                if (ObjWorkSheet.Cells[lineNumberAccessories, 2].Text.ToString() == "") break;
+                listBox1.Items.Add(ObjWorkSheet.Cells[lineNumberAccessories, 2].Text.ToString());
+                excelPar.Add("Модель клапана: " + ObjWorkSheet.Cells[lineNumberAccessories, 4].Text.ToString() +
+                            "\nМодель привода: " + ObjWorkSheet.Cells[lineNumberAccessories, 18].Text.ToString() +
+                            "\nКласс давления: " + ObjWorkSheet.Cells[lineNumberAccessories, 8].Text.ToString() + "PN , " +
+                            ObjWorkSheet.Cells[lineNumberAccessories, 9].Text.ToString() + " ASME" +
+                            "\nDN: " + ObjWorkSheet.Cells[lineNumberAccessories, 11].Text.ToString());
+                lineNumberAccessories++;
             }
             ObjWorkBook.Close(false, Type.Missing, Type.Missing);
             ObjWorkExcel.Quit();
@@ -232,73 +230,86 @@ namespace ClientClap
         }
 
         //todo StringBuilder
-        public async void create_Arm(Excel.Worksheet ObjWorkSheet, int numberString, string fileNameRezDvg)
+        public async void create_Arm(Excel.Worksheet ObjWorkSheet, int numberString, String fileNameRezDvg)
         {
-            string handString = "Ручной дублер";
-            string[] ram_par = {    "Масса, кг: ", "Заказчик: ", "Потребитель: ", "Установка: ", "Позиция: ",
+            // создаём строку для 
+            String handString = "Ручной дублер";
+            // массив для хранения параметров рамки 
+            String[] frameParameters = {    "Масса, кг: ", "Заказчик: ", "Потребитель: ", "Установка: ", "Позиция: ",
                                     "Модель привода: ", "Модель клапана: " , "Размер: DN " , "присоединение: PN " , "ХЗ1",
                                     "ХЗ2", "ХЗ3" , "ХЗ4" , "ХЗ5" , "ХЗ6"   };
 
-            
-            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+            // считываем класс давления
             int asme = Convert.ToInt32(ObjWorkSheet.Cells[numberString, 9].Text.ToString());
+
+            // считываем диаметр трубы
             int DN = Convert.ToInt32(ObjWorkSheet.Cells[numberString, 11].Text.ToString());
-            string form = ObjWorkSheet.Cells[numberString, 10].Text.ToString();
-            string[] priv = ObjWorkSheet.Cells[numberString, 18].Text.ToString().Split('/');
 
-            ram_par[1] += ObjWorkSheet.Cells[2, 3].Text.ToString();
-            ram_par[2] += ObjWorkSheet.Cells[3, 3].Text.ToString();
-            ram_par[3] += ObjWorkSheet.Cells[5, 3].Text.ToString();
-            ram_par[4] += ObjWorkSheet.Cells[numberString, 2].Text.ToString();
-            ram_par[5] += ObjWorkSheet.Cells[numberString, 18].Text.ToString();
-            ram_par[6] += ObjWorkSheet.Cells[numberString, 4].Text.ToString();
-            ram_par[7] += ObjWorkSheet.Cells[numberString, 11].Text.ToString();
-            ram_par[8] += ObjWorkSheet.Cells[numberString, 8].Text.ToString();
-            string[] numberPartsExcel = ObjWorkSheet.Cells[numberString, 28].Text.ToString().Split(',');
+            // форма присоединеия ( фланец или под приварку, если я правильно понял).
+            String form = ObjWorkSheet.Cells[numberString, 10].Text.ToString();
 
+            // считываем модель привода.
+            String[] actuator = ObjWorkSheet.Cells[numberString, 18].Text.ToString().Split('/');
+
+            // добавляем к параметрам их значение.
+            frameParameters[1] += ObjWorkSheet.Cells[2, 3].Text.ToString();
+            frameParameters[2] += ObjWorkSheet.Cells[3, 3].Text.ToString();
+            frameParameters[3] += ObjWorkSheet.Cells[5, 3].Text.ToString();
+            frameParameters[4] += ObjWorkSheet.Cells[numberString, 2].Text.ToString();
+            frameParameters[5] += ObjWorkSheet.Cells[numberString, 18].Text.ToString();
+            frameParameters[6] += ObjWorkSheet.Cells[numberString, 4].Text.ToString();
+            frameParameters[7] += ObjWorkSheet.Cells[numberString, 11].Text.ToString();
+            frameParameters[8] += ObjWorkSheet.Cells[numberString, 8].Text.ToString();
+            String[] numberPartsExcel = ObjWorkSheet.Cells[numberString, 28].Text.ToString().Split(',');
+
+            // тут мы ищем строку после которой перечисленны номаера принадлежностей и их названия.
             int start = 1;
             while (true)
             {
                 if (ObjWorkSheet.Cells[start, 1].Text.ToString() == "ПРИНАДЛЕЖНОСТИ") break;
                 start++;
             }
-            int fin = start;
+            int lineNumberAccessories = start;
             while (true)
             {
-                if (ObjWorkSheet.Cells[fin, 1].Text.ToString() == "") break;
-                fin++;
+                if (ObjWorkSheet.Cells[lineNumberAccessories, 1].Text.ToString() == "") break;
+                lineNumberAccessories++;
             }
 
-            string frontParts = "";
-            string backParts = "";
-            string hand = "";
+            // созаём пустые строки для частей и ручного привода.
+            String frontParts = "";
+            String backParts = "";
+            String hand = "";
+
+            // переменная для расчёта массы.
             int mas = 0;
             bool boolHand = false;
             int numberStr = 1;
-            for (int i = start + 1; i < fin; i++)
+            for (int i = start + 1; i < lineNumberAccessories; i++)
             {
-                string rez = ObjWorkSheet.Cells[i, 1].Text.ToString();
-                while (ObjWorkSheet.Cells[i + 1, 1].Text.ToString().IndexOf((numberStr + 1).ToString() + ". ") != 0 && i < fin)
+                String result = ObjWorkSheet.Cells[i, 1].Text.ToString();
+                while (ObjWorkSheet.Cells[i + 1, 1].Text.ToString().IndexOf((numberStr + 1).ToString() + ". ") != 0 && i < lineNumberAccessories)
                 {
                     i++;
-                    rez += " " + ObjWorkSheet.Cells[i, 1].Text.ToString().Replace("\"", "");
+                    result += " " + ObjWorkSheet.Cells[i, 1].Text.ToString().Replace('\"', null);
                 }
-                rez = rez.Substring(numberStr.ToString().Length + 2);
+                result = result.Substring(numberStr.ToString().Length + 2);
+                // тут распределение частей на те которые нужно вставлять перед приводои и те которые после, его нужно переделать
                 if (Array.IndexOf(numberPartsExcel, numberStr.ToString()) != -1)
                 {
-                    string numberPart = getFrontPart(rez);
+                    String numberPart = getFrontPart(result);
                     if (numberPart != "0")
                     {
                         frontParts += numberPart + ";";
                         mas += 5;
                     }
-                    numberPart = getBackPart(rez);
+                    numberPart = getBackPart(result);
                     if (numberPart != "0")
                     {
                         backParts += numberPart + ";";
                         mas += 5;
                     }
-                    if (rez == handString)
+                    if (result == handString)
                     {
                         hand = "5";
                         boolHand = true;
@@ -308,42 +319,24 @@ namespace ClientClap
             }
             
 
-            //todo remove
-            GC.Collect();
-
-            int[] values = { 20, 25, 40, 50, 80, 100, 150, 200 };
-            string parClap = getData(Array.IndexOf(values, DN), form.ToLower(), asme);
-            bool privPar = false;
-            if (priv[0] == "88") privPar = true;
-            ram_par[0] += (getWeight(Array.IndexOf(values, DN), asme) + getWeightPriv(Convert.ToInt32(priv[1]), boolHand) + mas).ToString();
-            string ram_par_str = "";
-            foreach (string i in ram_par)
+            // получаем размеры клапана
+            String valveParameters = getData(Array.IndexOf(values, DN), form.ToLower(), asme);
+            // вот тут я так и не вспомни почему 88, но скорее всего потому что только с приводами этой линейки мы работали
+            bool actuatorParameters = false;
+            if (actuator[0] == "88") actuatorParameters = true;
+            // считаем общию массу
+            frameParameters[0] += (getWeight(Array.IndexOf(values, DN), asme) + getWeightPriv(Convert.ToInt32(actuator[1]), boolHand) + mas).ToString();
+            // переделываем массив параметров рамки в строку 
+            String frameParametersString = "";
+            foreach (String i in frameParameters)
             {
-                ram_par_str += i + ";";
+                frameParametersString += i + ";";
             }
-            ram_par_str.Trim(';');
-            string s = await RequestAsync(fileNameRezDvg, hand, parClap, ram_par_str, getDataPriv(Convert.ToInt32(priv[1]), privPar), backParts.Trim(';'), frontParts.Trim(';'));
-            MessageBox.Show(s);
+            // убираем последнию точку с запятой из этой строки
+            frameParametersString.Trim(';');
+            String requestResult = await RequestAsync(fileNameRezDvg, hand, valveParameters, frameParametersString, getDataPriv(Convert.ToInt32(actuator[1]), actuatorParameters), backParts.Trim(';'), frontParts.Trim(';'));
+            MessageBox.Show(requestResult);
         }
-
-        /*
-private async void button1_Click(object sender, EventArgs e)
-{
-string s = await RequestAsync();
-MessageBox.Show(s);
-}
-private async Task<String> RequestAsync()
-{
-string rez = "asf";
-string url = "https://localhost:44394/Index?klap_par=25,35,15&klap=Клапан";
-WebRequest request = WebRequest.Create(url);
-WebResponse response = request.GetResponse();
-Stream stream = response.GetResponseStream();
-StreamReader reader = new StreamReader(stream);
-rez = reader.ReadToEnd();
-response.Close();
-return rez;
-}*/
     }
 
 }
